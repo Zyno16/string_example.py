6136eb13b1ffb33265a968faf053bd193eb7e3db
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-28 18:23:20 +0000

    Add files via upload

---
+++ example.string.1.py
@@ -0,0 +1,19 @@
+my ="""hello ahmed
+  how are you 
+how is your life
+"""
+print(my * 3)  #will print 3 times
+str ="\N{copyright sign}"
+print(str)
+str1 ="\N{registered sign}"
+print(str1)
+str2 ="\N{up down arrow}"
+print(str2)
+str3 ="\N{left right arrow}"
+print(str3)
+str5="\x41"
+print(str5)
+str6="\u0042"
+print(str6)
+str7 ="\U00000043"
+print(str)
